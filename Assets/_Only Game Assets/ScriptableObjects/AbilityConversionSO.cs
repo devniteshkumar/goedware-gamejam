aabff76ffced966f148643a4555c7676752f315c
VishalMaurya0 <vishalmaurya22.24@kgpian.iitkgp.ac.in>
2025-06-21 16:02:30 +0530

    SO made

---
+++ Assets/_Only Game Assets/ScriptableObjects/AbilityConversionSO.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "AbilityConversion", menuName = "Scriptable Objects/AbilityConversion")]
+public class AbilityConversionSO : ScriptableObject
+{
+    [SerializeField] public List<Conversion> Conversions = new();
+}
+
+[System.Serializable]
+public class Conversion
+{
+    public ResourceAmount startingResource;
+    public ResourceAmount convertedResource;
+}
+
+[System.Serializable]
+public class ResourceAmount
+{
+    public ResourceTypes recsource;
+    public float amount;
+}
---
+++ Assets/_Only Game Assets/Scripts/SpecialAbilityManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class SpecialAbilityManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+public enum ResourceTypes
+{
+    Time,
+    MovementSpeed,
+    AttackDamage,
+    AttackingRadius,
+    Health,
+    TimeFreeze,
+}
+
+//public class Resource
+//{
+//    public ResourceTypes resource;
+//    public float value;
+//}
