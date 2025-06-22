04c74f269340ba6d81f4a656d0fc040e69132e69
blaZe2603 <keshavb2603.24@kgpian.iitkgp.ac.in>
2025-06-23 02:37:45 +0530

    added arrow to enemy and also health ui

--- Assets/_Only Game Assets/Scripts/Enemies/basic_enemies.cs
+++ Assets/_Only Game Assets/Scripts/Enemies/basic_enemies.cs
@@ -1,24 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class basic_enemies : MonoBehaviour
 {
     public GameObject player;
+    [SerializeField] GameObject arrow;
     BoxCollider2D col;
-
+    bool shoot_arrow = true;
     Rigidbody2D rb;
 
     Quaternion rotation;
     Vector2 movedir;
 
-
+    public int maxHealth = 100;
+    public int currentHealth;
+    public HealthUI healthUI;
 
     [SerializeField] float range_of_player;
     [SerializeField] float speed;
+    [SerializeField] float arrow_speed;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        currentHealth = maxHealth;
+        if (healthUI != null)
+        {
+            healthUI.SetMaxHealth(maxHealth);
+            healthUI.SetHealth(currentHealth); // to initialize UI
+        }
         col = gameObject.GetComponent<BoxCollider2D>();
         rb = gameObject.GetComponent<Rigidbody2D>();
         player = GameObject.FindGameObjectWithTag("Player");
@@ -27,6 +39,12 @@ public class basic_enemies : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            var healthSystem = gameObject.GetComponent<HealthSystem>();
+            healthSystem.TakeDamage(20);
+            Debug.Log("a");
+        }     
         //either we can make it stop before hitting player and do a sword attack
         if (Vector2.Distance(transform.position, player.transform.position) > range_of_player)
         {
@@ -47,12 +65,22 @@ public class basic_enemies : MonoBehaviour
         movedir = player.transform.position - transform.position;
         movedir.Normalize();
         rotation = Quaternion.LookRotation(Vector3.forward, movedir);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, 200 * Time.deltaTime);
 
-        
-        
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, 200 * Time.deltaTime);
-        
-
+        if (transform.rotation == rotation && shoot_arrow)
+        {
+            StartCoroutine(fire());
+        }
     }
 
+    IEnumerator fire()
+    {
+        
+        GameObject bullet = Instantiate(arrow, gameObject.transform.position ,gameObject.transform.rotation);
+        shoot_arrow = false;
+        bullet.GetComponent<Rigidbody2D>().linearVelocity = transform.up * arrow_speed;
+        yield return new WaitForSeconds(4);
+        shoot_arrow = true;
+        Destroy(bullet);
+    }
 }
