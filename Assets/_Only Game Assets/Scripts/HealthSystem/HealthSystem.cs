f6161ec2441d1526a6190e8c79a171517242daa8
devniteshkumar <devniteshkumar96@gmail.com>
2025-06-22 13:45:09 +0530

    health scripts

---
+++ Assets/_Only Game Assets/Scripts/HealthSystem/HealthSystem.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class HealthSystem : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Heal(int amount)
+    {
+        currentHealth += amount;
+        if (currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+    }
+
+    void Die()
+    {
+        // Handle death logic here
+        Debug.Log("You died!");
+    }
+}
+
