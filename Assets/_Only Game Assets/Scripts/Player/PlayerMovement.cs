c6a8df8d481f015ed2b7320e9cf5f8ed2d1b84bb
devniteshkumar <devniteshkumar96@gmail.com>
2025-06-25 21:50:12 +0530

    player-animations complete

--- Assets/_Only Game Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/_Only Game Assets/Scripts/Player/PlayerMovement.cs
@@ -5,12 +5,14 @@ public class PlayerMovement : MonoBehaviour
 {
     public InputActionAsset inputActions;
 
-    private InputAction moveAction;
+    private InputAction moveAction, attackAction, defenseAction;
+    private Vector2 lastVelocity;
     private Vector2 moveInput;
     private Animator animator;
 
     public float moveSpeed = 5f;
     private Rigidbody2D rb;
+    private bool moving, attacking, defense;
 
     private void Awake()
     {
@@ -22,16 +24,47 @@ public class PlayerMovement : MonoBehaviour
     {
         var actionMap = inputActions.FindActionMap("Player");
         moveAction = actionMap.FindAction("Move");
+        attackAction = actionMap.FindAction("Attack");
+        defenseAction = actionMap.FindAction("Defense");
 
         moveAction.Enable();
+        attackAction.Enable();
 
-        moveAction.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
-        moveAction.canceled += ctx => moveInput = Vector2.zero;
+        moveAction.performed += ctx =>
+        {
+            moveInput = ctx.ReadValue<Vector2>();
+            moving = true;
+        };
+        moveAction.canceled += ctx =>
+        {
+            moveInput = Vector2.zero;
+            moving = false;
+        };
+
+        attackAction.performed += ctx =>
+        {
+            Attack();
+            attacking = true;
+        };
+        attackAction.canceled += ctx =>
+        {
+            attacking = false;
+        };
+        defenseAction.performed += ctx =>
+        {
+            Defense();
+            defense = true;
+        };
+        defenseAction.canceled += ctx =>
+        {
+            defense = false;
+        };
     }
 
     private void OnDisable()
     {
         moveAction.Disable();
+        attackAction.Disable();
     }
 
     private void FixedUpdate()
@@ -40,21 +73,31 @@ public class PlayerMovement : MonoBehaviour
     }
     public void SetAnimParameters(Vector2 velocity)
     {
-        bool idle = (velocity.x == 0 && velocity.y == 0);
-        animator.SetBool("idle", idle);
-        bool movingUp = velocity.y > 0;
-        animator.SetBool("movingUp", movingUp);
-        bool movingDown = velocity.y < 0;
-        animator.SetBool("movingDown", movingDown);
-        bool movingLeft = velocity.x < 0;
-        animator.SetBool("movingLeft", movingLeft);
-        bool movingRight = velocity.x > 0;
-        animator.SetBool("movingRight", movingRight);
+        if (velocity == Vector2.zero)
+        {
+            velocity = lastVelocity;
+        }
+        lastVelocity = velocity;
+        animator.SetBool("moving", moving);
+        animator.SetBool("attacking", attacking);
+        animator.SetBool("defense", defense);
+        animator.SetFloat("AnimMoveX", velocity.x);
+        animator.SetFloat("AnimMoveY", velocity.y);
+    }
+    private void Update()
+    {
+        Vector2 velocity = rb.linearVelocity;
+        SetAnimParameters(velocity);
+    }
+    private void Attack()
+    {
+        // Add attack logic here
+        Debug.Log("Attack performed!");
     }
 
-    void Update()
+    private void Defense()
     {
-        Vector2 velocity = GetComponent<Rigidbody2D>().linearVelocity;
-        SetAnimParameters(velocity);
+        // Add defense logic here
+        Debug.Log("Defense performed!");
     }
 }
