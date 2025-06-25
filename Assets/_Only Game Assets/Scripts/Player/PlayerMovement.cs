188d6a748163f590cce06ae51a591be4e206c355
devniteshkumar <devniteshkumar96@gmail.com>
2025-06-25 15:45:28 +0530

    added player movement

--- Assets/_Only Game Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/_Only Game Assets/Scripts/Player/PlayerMovement.cs
@@ -7,6 +7,7 @@ public class PlayerMovement : MonoBehaviour
 
     private InputAction moveAction;
     private Vector2 moveInput;
+    private Animator animator;
 
     public float moveSpeed = 5f;
     private Rigidbody2D rb;
@@ -14,6 +15,7 @@ public class PlayerMovement : MonoBehaviour
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     private void OnEnable()
@@ -35,5 +37,22 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         rb.linearVelocity = moveInput * moveSpeed;
+    }
+    public void SetAnimParameters(Vector2 velocity)
+    {
+        bool movingUp = velocity.y > 0;
+        animator.SetBool("movingUp", movingUp);
+        bool movingDown = velocity.y < 0;
+        animator.SetBool("movingDown", movingDown);
+        bool movingLeft = velocity.x < 0;
+        animator.SetBool("movingLeft", movingLeft);
+        bool movingRight = velocity.x > 0;
+        animator.SetBool("movingRight", movingRight);
+    }
+
+    void Update()
+    {
+        Vector2 velocity = GetComponent<Rigidbody2D>().linearVelocity;
+        SetAnimParameters(velocity);
     }
 }
