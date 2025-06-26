2ab8d4328469a6afefc21106a0f80bc0b8e00a3c
blaZe2603 <keshavb2603.24@kgpian.iitkgp.ac.in>
2025-06-26 19:05:26 +0530

    WIP: work in progress before scene recovery

--- Assets/_Only Game Assets/Scripts/Enemies/enemy_healer.cs
+++ Assets/_Only Game Assets/Scripts/Enemies/enemy_healer.cs
@@ -5,51 +5,50 @@ using Unity.VisualScripting;
 using Unity.Mathematics;
 
 public class enemy_healer : MonoBehaviour
-
 {
-    public int i;
-    quaternion rotation;
-    Vector2 movedir;
-    [SerializeField] GameObject close_enemy;
-    [SerializeField] GameObject archer_enemy;
+    public static List<GameObject> enemiesToHeal = new List<GameObject>(); // Shared globally
 
     GameObject current_enemy = null;
-    public Stack<GameObject> enemy_to_heal = new Stack<GameObject>();
-    // public Stack<GameObject> archer_enemy_to_heal = new Stack<GameObject>();
+    quaternion rotation;
+    Vector2 movedir;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame 
     void Update()
     {
         if (current_enemy == null)
         {
-            if (enemy_to_heal.Count != 0)
-            {
-                current_enemy = enemy_to_heal.Pop();
-            }
-            // else if (archer_enemy_to_heal.Count != 0)
-            // {
-            // current_enemy = archer_enemy_to_heal.Pop();
-            //     Debug.Log("a");
-            // }
+            current_enemy = FindNextEnemyToHeal();
         }
         else
         {
-            if (current_enemy.GetComponent<HealthSystem>().currentHealth != current_enemy.GetComponent<HealthSystem>().maxHealth && !current_enemy.GetComponent<HealthSystem>().dead )
+            HealthSystem hs = current_enemy.GetComponent<HealthSystem>();
+            if (hs.currentHealth < hs.maxHealth && !hs.dead)
             {
                 RotateEnemy(current_enemy);
-                // Debug.Log("healing");
-                current_enemy.GetComponent<HealthSystem>().Heal(10 * Time.deltaTime);
+                hs.Heal(10 * Time.deltaTime);
             }
             else
+            {
                 current_enemy = null;
+            }
         }
     }
+
+    GameObject FindNextEnemyToHeal()
+    {
+        foreach (GameObject enemy in enemiesToHeal)
+        {
+            if (enemy == null) continue;
+
+            HealthSystem hs = enemy.GetComponent<HealthSystem>();
+            if (hs != null && hs.currentHealth < hs.maxHealth && !hs.dead)
+            {
+                return enemy;
+            }
+        }
+
+        return null;
+    }
+
     void RotateEnemy(GameObject enemy)
     {
         movedir = enemy.transform.position - transform.position;
@@ -57,6 +56,4 @@ public class enemy_healer : MonoBehaviour
         rotation = Quaternion.LookRotation(Vector3.forward, movedir);
         transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, 200 * Time.deltaTime);
     }
-
-
 }
