f4e9872de66d8a343b4b433bd663b41b859e2a3a
VishalMaurya0 <vishalmaurya22.24@kgpian.iitkgp.ac.in>
2025-06-30 18:08:56 +0530

    Update EnemyWavesManager.cs

--- Assets/_Only Game Assets/Scripts/EnemyWaves/EnemyWavesManager.cs
+++ Assets/_Only Game Assets/Scripts/EnemyWaves/EnemyWavesManager.cs
@@ -60,6 +60,7 @@ public class EnemyWavesManager : MonoBehaviour
         if (LevelDone || pauseAll) return;
         if (time > totalWaveTime_Conatant && !LevelDone && allEnemiesInWave.Count <= 0)
         {
+            SceneController.instance.Complete();
             // SceneManager.LoadScene(GameManager.Instance.levels[++GameManager.Instance.currentScene]);
             GameManager.Instance.debugMessageTextToShow = "Level Complete!";
             LevelDone = true;
